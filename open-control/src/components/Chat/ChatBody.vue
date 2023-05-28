8195c91710a8db08bab66fb211069b94efc7adaa
inagamov <inagamov.tyoma@gmail.com>
2023-05-28 23:31:25 +0300

    chat styles update

--- open-control/src/components/Chat/ChatBody.vue
+++ open-control/src/components/Chat/ChatBody.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-scroll-area ref="chatArea" class="full-height" style="background-color: #FFF6F3;" >
+  <q-scroll-area ref="chatArea" class="full-height chat_background">
     <div class="q-gutter-md q-pa-md bubbles_container">
       <q-chat-message
         key="0000"
-        name='Помощник'
+        name="Помощник"
         :text="['Привет! Я робот Помощник\nЧем могу помочь?']"
-        text-color='white'
+        text-color="white"
       />
       <q-chat-message
         :key="key"
@@ -17,12 +17,11 @@
         :text-color="message.from === 'me' ? '#122B27' : 'white'"
       />
     </div>
-
   </q-scroll-area>
 </template>
 
 <script setup>
-import {watch, ref, toRefs, onBeforeMount, onMounted} from 'vue';
+import { watch, ref, toRefs, onBeforeMount, onMounted } from 'vue';
 const chatArea = ref(null);
 const props = defineProps({
   messages: {
@@ -31,27 +30,38 @@ const props = defineProps({
   },
   user: {
     type: Object,
-    default: null
+    default: null,
+  },
+});
+
+const { messages } = toRefs(props);
+
+watch(
+  () => messages,
+  () => {
+    const scrollTarget = chatArea.value?.getScrollTarget();
+    const duration = 300;
+    chatArea?.value?.setScrollPosition(
+      'vertical',
+      scrollTarget.scrollHeight,
+      duration
+    );
+  },
+  {
+    immediate: true,
+    deep: true,
   }
-});
-
-const { messages } = toRefs(props)
-
-watch(() => messages, () => {
-  const scrollTarget = chatArea.value?.getScrollTarget();
-  const duration = 300;
-  chatArea?.value?.setScrollPosition('vertical',scrollTarget.scrollHeight, duration);
-}, {
-  immediate: true,
-  deep: true,
-});
+);
 
 onMounted(() => {
   const scrollTarget = chatArea.value?.getScrollTarget();
   const duration = 300;
-  chatArea?.value?.setScrollPosition('vertical',scrollTarget.scrollHeight, duration);
-})
-
+  chatArea?.value?.setScrollPosition(
+    'vertical',
+    scrollTarget.scrollHeight,
+    duration
+  );
+});
 </script>
 
 <style scoped lang="scss">
@@ -59,21 +69,34 @@ onMounted(() => {
  * chat
  */
 ::v-deep(.q-message-text--received) {
-  background: #f3715f;
-  color: #FFFFFF;
+  background: linear-gradient(136.37deg, #f75d47 19.2%, #e13925 75.6%);
+  animation: gradient 1s ease-in-out infinite alternate;
+  color: white;
   border-radius: 15px 15px 15px 0;
 }
 
 ::v-deep(.q-message-text--sent) {
-  background: #FFFFFF;
+  background: rgba(255, 255, 255, 0.3);
+  backdrop-filter: blur(2px);
   color: $accent;
   border-radius: 15px 15px 0 15px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 10px 20px !important;
 }
 
 ::v-deep(.bubbles_container) {
-  font-size: 16px;
+  font-size: 14px;
   bottom: 0;
   left: 0;
 }
 
+.chat_background {
+  background-image: linear-gradient(
+      rgba(255, 255, 255, 0.8),
+      rgba(255, 255, 255, 0.8)
+    ),
+    url('/pattern.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+}
 </style>
--- open-control/src/components/Chat/ChatComponent.vue
+++ open-control/src/components/Chat/ChatComponent.vue
@@ -21,9 +21,4 @@
   padding-top: env(safe-area-inset-top);
   padding-bottom: env(safe-area-inset-bottom);
 }
-
-.blur {
-  background: rgba(255, 255, 255, 0.4);
-  backdrop-filter: blur(6px);
-}
 </style>
